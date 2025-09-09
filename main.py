1fbdd02923d5aa5b4892e76f11667e670ecc9100
JuanCobo <juan.cobo@baobabsoluciones.es>
2025-09-09 16:29:30 +0200

    Add initial project files including main.py, pyproject.toml, and CI workflows; set Python version to 3.12

---
+++ main.py
@@ -0,0 +1,15 @@
+"""Main runner file."""
+
+import datetime
+
+import pandas as pd
+
+if __name__ == "__main__":
+    sample_data = {
+        "name": ["John", "Jane", "Jim", "Jill"],
+        "age": [25, 30, 35, 40],
+        "city": ["New York", "Los Angeles", "Chicago", "Houston"],
+    }
+    sample_df = pd.DataFrame(sample_data)
+    timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    sample_df.to_csv(f"sample_data_{timestamp}.csv", index=False)
